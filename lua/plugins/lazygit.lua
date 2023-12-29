5fa293499a10eb3d74b0985debaaadcfe3336783
Steve <stevedsimkins@gmail.com>
2023-12-29 16:53:30 -0500

    keymap for lazygit

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,14 +1,15 @@
 return {
-	"kdheepak/lazygit.nvim",
-	config = function()
-		require("lazy").setup({
-			{
-				"kdheepak/lazygit.nvim",
-				-- optional for floating window border decoration
-				dependencies = {
-					"nvim-lua/plenary.nvim",
-				},
-			},
-		})
-	end,
+  "kdheepak/lazygit.nvim",
+  config = function()
+    require("lazy").setup({
+      {
+        "kdheepak/lazygit.nvim",
+        -- optional for floating window border decoration
+        dependencies = {
+          "nvim-lua/plenary.nvim",
+        },
+      },
+    })
+    vim.keymap.set("n", "<leader>lg", "<cmd>LazyGit<CR>", {})
+  end,
 }
