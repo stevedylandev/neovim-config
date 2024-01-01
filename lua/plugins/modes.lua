e187b720753e30153b2eebd6aca7ce7e2315dc8e
Steve <stevedsimkins@gmail.com>
2024-01-01 16:12:48 -0500

    updated modes

--- lua/plugins/modes.lua
+++ lua/plugins/modes.lua
@@ -1,17 +1,14 @@
 return {
-	"mvllow/modes.nvim",
-	dependencies = {
-		"RRethy/nvim-base16",
-	},
-	tag = "v0.2.0",
-	config = function()
-		require("modes").setup({
-			colors = {
-				copy = "#C1C1C1",
-				delete = "#E78A52",
-				visual = "#FBCB96",
-				insert = "#5F8787",
-			},
-		})
-	end,
+  "mvllow/modes.nvim",
+  tag = "v0.2.0",
+  config = function()
+    require("modes").setup({
+      colors = {
+        copy = vim.g.terminal_color_2,
+        delete = vim.g.terminal_color_1,
+        visual = vim.g.terminal_color_3,
+        insert = vim.g.terminal_color_6,
+      },
+    })
+  end,
 }
